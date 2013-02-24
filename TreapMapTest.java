bd206744a1f36994e0eb24a6dd558b3a823242eb
zheltopuzik <zheltopuzik@rambler.ru>
2013-02-25 00:06:46 +0600

    Update TreapMapTest.java

--- TreapMapTest.java
+++ TreapMapTest.java
@@ -1,9 +1,9 @@
 import junit.framework.TestCase;
 
 
-public class TreapMapTest extends TestCase
+public class TreapMapTest extends TestCase 
 {
-  
+	
 	public void test1()
 	{
 		TreapMap<Integer, String> currentTreapMap = new TreapMap<Integer, String>();
@@ -12,13 +12,14 @@ public class TreapMapTest extends TestCase
 		currentTreapMap.put(1, "one");
 		currentTreapMap.remove(1);
 		currentTreapMap.put(1, "one1");
+		currentTreapMap.put(1, "one2");
 		currentTreapMap.remove(3);	
 		currentTreapMap.remove(6);
 		currentTreapMap.put(6, "six1");
-		
-		assertEquals(currentTreapMap.get(1), "one1");
+		currentTreapMap.put(6, "six2");
+		assertEquals(currentTreapMap.get(1), "one2");
 		assertEquals(currentTreapMap.get(5), "five");
-		assertEquals(currentTreapMap.get(6), "six1");
+		assertEquals(currentTreapMap.get(6), "six2");
 		assertNull(currentTreapMap.get(7));
 	}
 	
@@ -30,60 +31,47 @@ public class TreapMapTest extends TestCase
 			currentTreapMap.put(i, i);
 		
 		for (Integer i = 1; i <= 100000; i++)
-			assertEquals (currentTreapMap.get(i), i);	
+			currentTreapMap.put(i, i+1);
+		
+		for (Integer i = 1; i <= 100000; i++)
+		{
+			Integer j = i + 1;
+			assertEquals (currentTreapMap.get(i), j);	
+		}
+		
+		for (Integer i = 1; i <= 100000; i++)
+			currentTreapMap.remove(i);
+		
+		for (Integer i = 1; i <= 100000; i++)
+			assertNull (currentTreapMap.get(i));
+				
 	}
 		
-	public void test3()
+	public void test3() throws RuntimeException
 	{
-		TreapMap<Integer, String> currentTreapMap = new TreapMap<Integer, String>();		
-		currentTreapMap.putNextPriority(2);
-		currentTreapMap.put(5, "5");
-		
-		currentTreapMap.putNextPriority(4);
-		currentTreapMap.put(3, "3");
-		
-		currentTreapMap.putNextPriority(1);
-		currentTreapMap.put(10, "10");
-		
-		currentTreapMap.remove(3);
-		
-		assertEquals (currentTreapMap.get(10), "10");
-		assertNull(currentTreapMap.get(3));
-		
-	}
+		TreapMap<Integer, Double> currentTreapMap = new TreapMap<Integer, Double>();
+		try
+		{
+			currentTreapMap.put(5, 5.0);	
+			assertEquals(currentTreapMap.size(), 1);
+			
+			currentTreapMap.put(6, 6.2);
+			assertEquals(currentTreapMap.containsValue(6.2), true);
+			
+			currentTreapMap.put(6, 6.5);
+			assertEquals(currentTreapMap.get(6), 6.5);
+			currentTreapMap.remove(5);
+			assertEquals(currentTreapMap.isEmpty(), false);
+			
+			currentTreapMap.remove(6);
 	
-	public void test4()
-	{
-		TreapMap<Integer, String> currentTreapMap = new TreapMap<Integer, String>();		
-		currentTreapMap.putNextPriority(8);
-		currentTreapMap.put(6, "6");
-		
-		currentTreapMap.putNextPriority(6);
-		currentTreapMap.put(5, "5");
-		
-		currentTreapMap.putNextPriority(7);
-		currentTreapMap.put(4, "4");
-		
-		currentTreapMap.putNextPriority(4);
-		currentTreapMap.put(3, "3");
+			assertEquals(currentTreapMap.isEmpty(), true);
+			assertNull (currentTreapMap.get(5));
 
-		currentTreapMap.putNextPriority(10);
-		currentTreapMap.put(2, "2");
-		
-		currentTreapMap.putNextPriority(5);
-		currentTreapMap.put(1, "1");
-
-		currentTreapMap.remove(6);
-		
-		assertEquals (currentTreapMap.get(4), "4");
-		
-		currentTreapMap.remove(4);
-		currentTreapMap.remove(2);
-		
-		assertEquals (currentTreapMap.get(5), "5");
-		assertEquals (currentTreapMap.get(1), "1");
-		assertNull(currentTreapMap.get(2));
-		assertNull(currentTreapMap.get(4));		
+		}
+		catch (RuntimeException msg)
+		{
+			System.out.println("Exception: " + msg);
+		}
 	}
-	
 }
